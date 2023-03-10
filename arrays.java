dfd23090ed3572fd2be9eedea914b4960c0668b2
udhayakumar v n <104062640+umeshudhaya@users.noreply.github.com>
2023-03-10 23:56:37 +0530

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,15 @@
+class arrays
+{
+   public static void main(String[] args)
+   {
+		int[] num ={12,22,32,33,44};
+		 String [] names={"vijay","ajith","rajini","kamal","dhanush"};
+		 double []  value={12.5,33.4,55.5,66.6,77.7};
+		  
+		  for(int i=0;i<value.length;i++)
+		  {  
+			 System.out.println(num[i]+"  "+names[i]+"  "+value[i]);
+		  } 
+	} 
+}	
+	
